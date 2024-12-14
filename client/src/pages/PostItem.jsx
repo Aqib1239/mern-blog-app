f4124ad4b93337c84ea674925784a406b8ab1123
Aqib1239 <114600200+Aqib1239@users.noreply.github.com>
2024-12-14 10:58:01 +0530

    Update PostItem.jsx

--- client/src/pages/PostItem.jsx
+++ client/src/pages/PostItem.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PostAuthor from './PostAuthor';
 
@@ -8,8 +7,13 @@ const PostItem = ({ postID, category, title, desc, authorID, thumbnail, createdA
   const postTitle = title ? (title.length > 30 ? title.substr(0, 30) + '...' : title) : "Untitled";
   const postCategory = category || "Uncategorized";
 
-  // Construct the image URL with fallback
-  const imageUrl = thumbnail ? `${process.env.REACT_APP_ASSETS_URL}/uploads/${thumbnail}` : 'path/to/default/image.jpg';
+
+  // Construct the image URL with conditional logic
+  const imageUrl = thumbnail ? 
+  (thumbnail.includes("res.cloudinary.com") ? 
+    thumbnail : 
+    `${process.env.REACT_APP_ASSETS_URL}/uploads/${thumbnail}`
+  ) : 'path/to/default/image.jpg';
 
   return (
     <article className='post'>
